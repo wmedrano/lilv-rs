63ecf05bd91cffd2ed4d664f4b116db9f7caa313
Will Medrano <will.s.medrano@gmail.com>
2021-10-21 17:05:51 -0700

    Enable building on docs.rs

--- build.rs
+++ build.rs
@@ -1,6 +1,11 @@
+use std::env;
+
 fn main() {
+    if env::var("DOCS_RS") == Ok("1".to_string()) {
+        return;
+    }
     pkg_config::Config::new()
         .atleast_version("0.24.2")
         .probe("lilv-0")
-        .unwrap();
+        .expect("lilv-0 could not be found with pkg_config.");
 }
