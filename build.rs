913121bdca1f6eeda4ba55ce33bc750fd00b278a
Cam Sexton <32211826+ctsexton@users.noreply.github.com>
2023-01-29 21:10:26 -0800

    link serd (#15)

--- build.rs
+++ build.rs
@@ -8,4 +8,8 @@ fn main() {
         .atleast_version("0.24.2")
         .probe("lilv-0")
         .expect("lilv-0 could not be found with pkg_config.");
+    pkg_config::Config::new()
+        .atleast_version("0.30.0")
+        .probe("serd-0")
+        .expect("serd-0 could not be found with pkg_config.");
 }
